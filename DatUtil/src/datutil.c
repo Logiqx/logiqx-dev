d4f5872be3f72a1732f87b4933eb57bfbe6be8ea
Logiqx <software@mikeg.me.uk>
2004-10-26 21:58:41 +0000

    Compiled v2.8, DatLib v1.11 (Zinc support)

--- DatUtil/src/datutil.c
+++ DatUtil/src/datutil.c
@@ -7,8 +7,8 @@
 
 /* --- Version information --- */
 
-#define DATUTIL_VERSION "v2.7"
-#define DATUTIL_DATE "18 August 2004"
+#define DATUTIL_VERSION "v2.8"
+#define DATUTIL_DATE "26 October 2004"
 
 
 /* --- The standard includes --- */
