ea7e6b3025c417838c517203a380285ee5cdb077
Logiqx <software@mikeg.me.uk>
2005-01-11 00:47:47 +0000

    First version, want a backup!

---
+++ Limited Edition/IPMask/src/ipmask.h
@@ -0,0 +1,5 @@
+/* --------------------------------------------------------------------------
+ * IPMask - Written by Logiqx (http://www.logiqx.com/)
+ *
+ * A handy utility for maintaining the IP masks at the MAME Testers FTP
+ * -------------------------------------------------------------------------- */
