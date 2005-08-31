a81d983663092929c285a1451e2814a27c7d0018
Logiqx <software@mikeg.me.uk>
2005-08-31 18:34:39 +0000

    DatUtil v2.18

--- DatUtil/src/datutil.c
+++ DatUtil/src/datutil.c
@@ -7,8 +7,8 @@
 
 /* --- Version information --- */
 
-#define DATUTIL_VERSION "v2.17"
-#define DATUTIL_DATE "10 April 2005"
+#define DATUTIL_VERSION "v2.18"
+#define DATUTIL_DATE "31 August 2005"
 
 
 /* --- The standard includes --- */
