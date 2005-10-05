9846d4704413f8edadccdceffcbf913d1530a39d
Logiqx <software@mikeg.me.uk>
2005-10-05 20:44:42 +0000

    Synthetic commit for incomplete tag DatUtil-v2-20

--- ROMBuild Tools/ROMDiff/src/romdiff.h
+++
@@ -1,7 +0,0 @@
-/* --------------------------------------------------------------------------
- * ROMDiff - Written by Logiqx (http://www.logiqx.com/)
- *
- * A simple little utility for re-building ROMs
- * -------------------------------------------------------------------------- */
-
-int compare(char *fn1, char *fn2);
