4657c9b88893ad775061644dd18b022bf6d5321e
Logiqx <software@mikeg.me.uk>
2005-10-28 21:37:53 +0000

    Was missing txt

--- DatLib/_zip_datlib.bat
+++ DatLib/_zip_datlib.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
 rm -f datlib.zip
-zip -r datlib.zip src makefile -x src/CVS/* src/*/CVS/* src/*/*/CVS/* 
+zip -r datlib.zip src makefile datlib.txt -x src/CVS/* src/*/CVS/* src/*/*/CVS/* 
 
 PAUSE
