3fa9416ea5708a8394045b1c6d6b957dbe650a2a
Logiqx <software@mikeg.me.uk>
2005-12-06 22:13:46 +0000

    Synthetic commit for incomplete tag DatLib-v2-13

--- MAMEDiff/_zip_mdiff.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-
-rm -f mdiff.zip
-zip -r mdiff.zip src makefile mamediff.exe mamediff.txt -x src/CVS/*
-
-PAUSE
--- ROMInfo/_zip_rinfo.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-
-rm -f rinfo.zip
-zip -r rinfo.zip src makefile rominfo.exe rominfo.ini rominfo.txt -x src/CVS/*
-
-PAUSE
