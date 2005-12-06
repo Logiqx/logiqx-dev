1aa991c646952844318895eb5c82883435877aff
Logiqx <software@mikeg.me.uk>
2005-12-06 21:59:31 +0000

    Synthetic commit for incomplete tag DatUtil-v2-25

--- DatLib/_zip_datlib.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-
-rm -f datlib.zip
-zip -r datlib.zip src makefile datlib.txt -x src/CVS/* src/*/CVS/* src/*/*/CVS/* 
-
-PAUSE
--- ZIPIdent/_zip_zident.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-
-rm -f zident.zip
-zip -r zident.zip src makefile zipident.exe zipident.txt -x src/CVS/*
-
-PAUSE
