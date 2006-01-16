47054bb379d27d4036ceb483a889a0fba60be25e
Logiqx <software@mikeg.me.uk>
2006-01-16 13:17:51 +0000

    Synthetic commit for incomplete tag MAMEDiff-v2-24

--- DatUtil/_zip_dutil.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-
-rm -f dutil.zip
-zip -r dutil.zip src makefile datutil.exe readme.txt whatsnew.txt -x src/CVS/*
-
-PAUSE
--- ROMBuild/_zip_rbuild.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-
-rm -f rbuild.zip
-zip -r rbuild.zip src makefile rombuild.exe rombuild.txt roms_in roms_out -x src/CVS/* roms_in/CVS/* roms_out/CVS/*
-
-PAUSE
