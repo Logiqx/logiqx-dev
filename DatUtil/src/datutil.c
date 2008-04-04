f47939fafb1c4b0fade0ab03cc31642634889c17
Logiqx <software@mikeg.me.uk>
2008-04-04 16:13:15 +0000

    DatUtil v2.36

--- DatUtil/src/datutil.c
+++ DatUtil/src/datutil.c
@@ -8,7 +8,7 @@
 /* --- Version information --- */
 
 #define DATUTIL_VERSION "v2.36"
-#define DATUTIL_DATE "Private Beta"
+#define DATUTIL_DATE "4 April 2008"
 
 
 /* --- The standard includes --- */
