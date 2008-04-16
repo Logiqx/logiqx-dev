16baf0e3a5578718be7b7eb0a89369fb6a303bfe
Logiqx <software@mikeg.me.uk>
2008-04-16 07:26:33 +0000

    Synthetic commit for incomplete tag DatUtil-v2-38

--- ROMBuild Tools/ROMBChk/src/rombchk.h
+++
@@ -1,9 +0,0 @@
-/* --------------------------------------------------------------------------
- * ROMBChk - Written by Logiqx (http://www.logiqx.com/)
- *
- * Support script for ROMBuild
- * -------------------------------------------------------------------------- */
-
-/* --- Function prototypes --- */
-
-int process_dat(char *fn);
