e6a70c34ef0f2a3d8b09a6c3fc006faeb6acdac0
Logiqx <software@mikeg.me.uk>
2008-05-05 09:43:06 +0000

    Renamed VBEmu datafile

--- ROMBuild Tools/ROMBChk/go.bat
+++ ROMBuild Tools/ROMBChk/go.bat
@@ -8,7 +8,7 @@ copy %CAESAR%\data\games\linux\*.dat
 copy %CAESAR%\data\games\mac\*.dat
 copy %CAESAR%\data\games\beos\*.dat
 copy %CAESAR%\data\games\arm\*.dat
-copy "VBEmu v1.00a.dat"+"%CAESAR%\data\games\multi2\VBEmu Screen Saver v1.01a - v1.00a.dat" "VBEmu Screen Saver v1.01a.dat"
+copy "VBEmu_v1.00a.dat"+"%CAESAR%\data\games\multi2\VBEmu_Screen_Saver_v1.01a.dat" "VBEmu_Screen_Saver_v1.01a.dat"
 
 rombchk MAME*dat
 
