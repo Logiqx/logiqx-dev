6f582e4e23e83caae6d7ced1a960a4ed1ce0ab12
Logiqx <software@mikeg.me.uk>
2008-11-02 11:33:29 +0000

    Synthetic commit for incomplete tag DatUtil-v2-44

--- Limited Edition/IPMask/src/ipmask.h
+++
@@ -1,5 +0,0 @@
-/* --------------------------------------------------------------------------
- * IPMask - Written by Logiqx (http://www.logiqx.com/)
- *
- * A handy utility for maintaining the IP masks at the MAME Testers FTP
- * -------------------------------------------------------------------------- */
