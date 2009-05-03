4eca09a0a648b90adf45c752ffe037e30d1dec4c
Logiqx <software@mikeg.me.uk>
2009-05-03 07:22:12 +0000

    RomBuild v2.13

--- ROMBuild/src/rombuild.c
+++ ROMBuild/src/rombuild.c
@@ -7,7 +7,7 @@
 /* --- Version information --- */
 
 #define ROMBUILD_VERSION "v2.13"
-#define ROMBUILD_DATE "Private Beta"
+#define ROMBUILD_DATE "3 May 2009"
 
 
 /* --- The standard includes --- */
